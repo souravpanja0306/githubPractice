318e41f47bdc4e0791342347913c50e4f0e21440
souravpanja <panjasourav03@gmail.com>
2024-09-29 00:37:56 +0530

    Navbar Created

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,13 +5,20 @@ const Navbar = () => {
     return (
         <div className='flex justify-around items-center bg-slate-700 w-full'>
             <div className='p-2 cursor-pointer hover:text-slate-400 text-white'>
-                <h1>Notional System Private Limited</h1>
+                <Link to="/">
+                    <h1>Notional System Private Limited</h1>
+                </Link>
             </div>
             <div>
                 <ul className='flex'>
                     <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/">Home</Link></li>
-                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/">Contact</Link></li>
-                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/">About</Link></li>
+                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/contact">Contact</Link></li>
+                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/about">About</Link></li>
+                </ul>
+            </div>
+            <div>
+                <ul className='flex'>
+                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/">Sign In</Link></li>
                 </ul>
             </div>
         </div>
