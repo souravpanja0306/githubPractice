3378c75ae5aceeb30694f0fd632ecf91ee1d3cf9
souravpanja <panjasourav03@gmail.com>
2024-09-29 00:52:00 +0530

    Homepage work started

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,11 +3,19 @@ import Layout from '../Components/Layout'
 
 const Home = () => {
   return (
-    <div>
-      <Layout>
-        Hello Home
-      </Layout>
-    </div>
+    <Layout>
+      <div className='flex min-h-screen'>
+        <div className='w-[15%]'>
+
+        </div>
+        <div className='w-[65%] bg-green-200'>
+
+        </div>
+        <div className='w-[20%] bg-slate-200'>
+
+        </div>
+      </div>
+    </Layout>
   )
 }
 
