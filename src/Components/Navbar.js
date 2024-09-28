3854da761789392c44ef1147637fe4635fdf460c
souravpanja <panjasourav03@gmail.com>
2024-09-28 19:58:22 +0530

    Navbar design complete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='flex justify-around items-center bg-slate-700 w-full'>
+            <div className='p-2 cursor-pointer hover:text-slate-400 text-white'>
+                <h1>Notional System Private Limited</h1>
+            </div>
+            <div>
+                <ul className='flex'>
+                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/">Home</Link></li>
+                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/">Contact</Link></li>
+                    <li className='p-2 cursor-pointer hover:text-slate-400 text-white'><Link to="/">About</Link></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
