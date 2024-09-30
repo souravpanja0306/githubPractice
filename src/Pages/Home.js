c01e89805b032eacf63b4097fba1a61336336e2f
souravpanja <panjasourav03@gmail.com>
2024-09-30 22:46:28 +0530

    Setup home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../Components/Layout'
 
 const Home = () => {
   return (
     <Layout>
-      <div className='flex min-h-screen'>
-        <div className='w-[15%]'>
-
-        </div>
-        <div className='w-[65%] bg-green-200'>
-
-        </div>
-        <div className='w-[20%] bg-slate-200'>
-
-        </div>
-      </div>
+      Hello Home
     </Layout>
   )
 }
